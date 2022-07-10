6051eb01ce6dd0464c57874edaa990a8e73dcd2a
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-07-10 18:53:08 +0300

    Lesson4 base commit

---
+++ src/main/java/ru/gb/spring1lesson6/Spring1Lesson6Application.java
@@ -0,0 +1,13 @@
+package ru.gb.spring1lesson6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1Lesson6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring1Lesson6Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/spring1lesson6/Spring1Lesson6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring1lesson6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1Lesson6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
